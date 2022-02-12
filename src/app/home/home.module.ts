ab1807b719d9e915d3ee462e27f4b90f80d0da1e
Christian Wansart <cwansart@users.noreply.github.com>
2022-02-12 10:55:31 +0100

    refactor home and add navigation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedModule } from '@shared/shared.module';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
