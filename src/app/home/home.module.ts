aaf6b0a3ca2404159b986b37f9a6baf03249927b
Christian Wansart <cwansart@users.noreply.github.com>
2022-02-12 09:57:25 +0100

    fix shared import

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '@shared/shared.module';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
