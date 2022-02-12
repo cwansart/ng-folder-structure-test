959ff371e26e356086a57de5e09ade5290316441
Christian Wansart <cwansart@users.noreply.github.com>
2022-02-12 10:07:57 +0100

    add input properties to TextInputComponent

--- src/app/shared/text-input/text-input.component.ts
+++ src/app/shared/text-input/text-input.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.scss'],
 })
-export class TextInputComponent {}
+export class TextInputComponent implements OnChanges {
+  @Input()
+  label: string;
+
+  @Input()
+  type: 'text';
+
+  @Input()
+  value: string;
+
+  id: string;
+
+  ngOnChanges() {
+    this.id = this.label.toLowerCase().replace(' ', '-') + '-text-input';
+  }
+}
